2c6cbe83823dfe298f86749484aa2ec5775d8de2
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-02-17 23:58:32 -0500

    Sanity tested swift mac app with core data

---
+++ MemoryWall/MemoryWall/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  MemoryWall
+//
+//  Created by Nikhil Verma on 2/17/17.
+//  Copyright © 2017 Nikhil Verma. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
