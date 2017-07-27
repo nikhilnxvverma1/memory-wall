8f83292730d3c87124fab908b4ecabdf81da6abb
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-07-27 22:13:25 +0530

    Added press drag release processor protocol

---
+++ MemoryWall/MemoryWall/PressDragReleaseProcessor.swift
@@ -0,0 +1,16 @@
+//
+//  PressDragReleaseProcessor.swift
+//  MemoryWall
+//
+//  Created by Nikhil Verma on 7/27/17.
+//  Copyright © 2017 Nikhil Verma. All rights reserved.
+//
+
+import Foundation
+
+
+protocol PressDragReleaseProcessor {
+	func mousePressed()
+	func mouseDragged()
+	func mouseReleased()
+}
