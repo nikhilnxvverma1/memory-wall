b4bc48bb69c0867bd3435ddcd4f2798691a1cd82
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-07-28 20:10:21 +0530

    Added empty create rectangle command conforming to needful protocols

---
+++ MemoryWall/MemoryWall/CreateRectangle.swift
@@ -0,0 +1,54 @@
+//
+//  CreateRectangle.swift
+//  MemoryWall
+//
+//  Created by Nikhil Verma on 7/28/17.
+//  Copyright © 2017 Nikhil Verma. All rights reserved.
+//
+
+import Foundation
+
+class CreateRectangle: Command,PressDragReleaseProcessor {
+	
+	private var workspace:Workspace
+	private var wall:Wall
+	private var previousSelection:Wall?
+	
+	init(workspace:Workspace,name:String){
+		self.workspace=workspace;
+		wall=Wall()
+		wall.name=name
+		
+		//get the previous wall if exists, if doesn't get the last wall in list, if possible
+		previousSelection = workspace.selectedWall != nil ? workspace.selectedWall : workspace.wallList.count>0 ? workspace.wallList.last : nil
+	}
+	
+	func execute(){
+		
+	}
+	
+	func unExecute(){
+		
+	}
+	
+	func getName()->String{
+		return "Create Rectangle"
+	}
+	
+	func updateAffectedViews(afterExecution:Bool){
+		
+	}
+	
+	func mousePressed(){
+		
+	}
+	
+	func mouseDragged(){
+		
+	}
+	
+	func mouseReleased(){
+		
+	}
+	
+}
